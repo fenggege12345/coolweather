85a6e13e99d1696b8faac3ffcc9fa042366afa78
Liugf <937413907@qq.com>
2019-02-19 17:11:24 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/android/db/Prnvince.java
@@ -0,0 +1,33 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Prnvince extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
